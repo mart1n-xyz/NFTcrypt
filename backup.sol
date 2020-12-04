3767050fa7951b02e8de0193666d9629b2637e93
martin112112 <20109376+martin112112@users.noreply.github.com>
2020-12-04 19:14:18 +0100

    optimized for contract size, basic MVP without logs

--- backup.sol
+++ backup.sol
@@ -18,9 +18,9 @@ contract NFTcrypt is Ownable {
 
 
 
-    function createChild(string memory name, string memory abb) public payable {
+    function createChild(string memory name, string memory abb, string memory ownerNm) public payable {
         address newOwner = msg.sender;
-        address issueContract = address((new Child).value(msg.value)(name, abb, newOwner));
+        address issueContract = address((new Child).value(msg.value)(name, abb, newOwner, registry, ownerNm));
         emit LogCreatedChild(msg.sender, name, abb, issueContract, newOwner);
         _deployedSet[msg.sender].add(issueContract);
     }
@@ -31,170 +31,167 @@ contract NFTcrypt is Ownable {
 
 contract Child is ERC721,Ownable {
 
-   //event LogCreatedBy(address creator, string name, string ticker);
-   // address public encKeyReg;
-   //string public ownerName;
+   event LogCreatedBy(address creator, string name, string ticker);
+   event LogEncKeyRead(address target, bytes key, bool status);
 
-   constructor (string memory name, string memory abb, address newOwner) ERC721(name, abb) public payable {
+   address public encKeyReg;
+   string public ownerName;
+
+   constructor (string memory name, string memory abb, address newOwner, address registry, string memory ownerNm) ERC721(name, abb) public payable {
         transferOwnership(newOwner);
-        //encKeyReg = registry;
-        //ownerName = ownerNm;
-        //emit LogCreatedBy(msg.sender, name, abb);
+        encKeyReg = registry;
+        ownerName = ownerNm;
+        emit LogCreatedBy(msg.sender, name, abb);
    }
-   //string ownerName;
+
    uint256 maxIndex = 0;
    uint256 lastBatch = 0;
 
    mapping (uint256 => uint256) public batchReg;
    mapping (uint256 => EnumerableSet.UintSet) _batchSet;
    mapping (uint256 => uint256) public batchMax;
-   //mapping (uint256 => string) private _encrSecret;
-   mapping (uint256 => string) public _secretHash;
+   mapping (uint256 => string) private _encrSecret;
+   mapping (uint256 => string) private _secretHash;
   // mapping (uint256 => bool) private _batchIdentical;
-   //mapping (uint256 => address) private purchased;
-   //mapping (address => EnumerableSet.UintSet) private _boughtTokens;
+   mapping (uint256 => address) private purchased;
+   mapping (address => EnumerableSet.UintSet) private _boughtTokens;
    mapping (uint256 => uint256) public origPrice;
    mapping (uint256 => bool) public forSale;
 
-   modifier tokenExists(uint256 tokenId) {
-     require(_exists(tokenId), "ERC721:  nonexistent token");
-     _;
-   }
-/**
-   function getOwnerName() public view returns (string memory) {
-     return ownerName;
-   }
+   //function getOwnerName() public view returns (string memory) {
+  //   return ownerName;
+  // }
    function setOwnerName(string memory ownNme) public onlyOwner {
      ownerName=ownNme;
-   }**/
+   }
+
    function _issueNFT(uint256 number, uint256 batch) internal {
      uint256 i =1;
      while (i<= number) {
        maxIndex++;
        _safeMint(msg.sender,maxIndex);
+
        batchReg[maxIndex]=batch;
        _batchSet[batch].add(maxIndex);
         i++;
      }
    }
+
    function createNewBatch(uint256 numberOfNft) onlyOwner public  {
      uint256 batchId = lastBatch+1;
      _issueNFT(numberOfNft,batchId);
      lastBatch = batchId;
      batchMax[batchId] = numberOfNft;
    }
-   /**
+
    function batchExists(uint256 batchId) public view returns (bool) {
      if (lastBatch>=batchId) {
        return true;
      } else {
        return false;
      }
-   } */
+   }
 
    function addToBatch(uint256 numberOfNft, uint256 batchId) onlyOwner public  {
-     require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
+     require(batchExists(batchId), "ERC721: operator query for nonexistent batch");
      _issueNFT(numberOfNft,batchId);
      batchMax[batchId] = batchMax[batchId] + numberOfNft;
    }
 
    function batchSize(uint256 batchId) public view returns (uint256)  {
-     require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
-     return batchMax[batchId];
+     require(batchExists(batchId), "ERC721: operator query for nonexistent batch");
+     uint256 size = batchMax[batchId];
+     return size;
    }
 
    function setBatchTokenURI(uint256 batchId, string memory _tokenURI) onlyOwner public  {
-    require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
+    uint256 size = batchSize(batchId);
     uint256 _i = 0;
-    while (_i<batchMax[batchId]) {
+    while (_i<size) {
       _setTokenURI(_batchSet[batchId].at(_i), _tokenURI);
       _i++;
     }
    }
 
-   function newBatchWithURI(uint256 numberOfNft,string memory _tokenURI) onlyOwner public {
-     createNewBatch(numberOfNft);
-     setBatchTokenURI(lastBatch,_tokenURI);
-   }
-
-   function setTokenURI(uint256 tokenId, string memory _tokenURI) public tokenExists(tokenId) onlyOwner {
+   function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
      _setTokenURI(tokenId,_tokenURI);
    }
-/**
+
    function getBatch(uint256 tokenId) public view returns (uint256) {
      return batchReg[tokenId];
 
-   } **/
+   }
 
 
 
-   /** function setEncSecret(uint256 tokenId, string memory secret) public tokenExists(tokenId) onlyOwner {
+   function setEncSecret(uint256 tokenId, string memory secret) public onlyOwner {
      _encrSecret[tokenId] = secret;
-   }**/
-    function setSecretHash(uint256 tokenId, string memory hashed) public onlyOwner {
+   }
+   function setSecretHash(uint256 tokenId, string memory hashed) public onlyOwner {
      _secretHash[tokenId] = hashed;
    }
-/**
-   function viewEncSecret(uint256 tokenId) public view tokenExists(tokenId) returns (string memory) {
+
+   function viewEncSecret(uint256 tokenId) public view returns (string memory) {
      return _encrSecret[tokenId];
-   }**/
-   /**
+   }
    function viewSecretHash(uint256 tokenId) public view returns (string memory) {
      return _secretHash[tokenId];
-   } **/
-/**
-    function buyer(uint256 tokenId) public view returns(address) {
+   }
+
+   function buyer(uint256 tokenId) public view returns(address) {
      return purchased[tokenId];
    }
-   **/
 
 
-   function getPrice(uint256 tokenId) public view tokenExists(tokenId) returns(uint256) {
+   function price(uint256 tokenId) public view returns(uint256) {
      require(forSale[tokenId]==true);
      return  origPrice[tokenId];
    }
-   function checkForSale(uint256 tokenId) public view tokenExists(tokenId) returns (bool) {
+   function checkForSale(uint256 tokenId) public view returns (bool) {
      if (forSale[tokenId]==true) {
        return true;
      } else {
        return false;}
    }
-   function setPrice(uint256 tokenId, uint256 price) public tokenExists(tokenId) onlyOwner {
-     require(ownerOf(tokenId)==msg.sender);
+   function setPrice(uint256 tokenId, uint256 price) public onlyOwner {
      origPrice[tokenId]=price;
-     forSale[tokenId]=true;
    }
-
-   function setBatchPrice(uint256 batchId, uint256 price) onlyOwner public  {
-    require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
-    uint256 _i = 0;
-    while (_i<batchMax[batchId]) {
-      setPrice(_batchSet[batchId].at(_i), price);
-      _i++;
-    }
+   function purchase(uint256 tokenId) public payable {
+     require( forSale[tokenId]==true && msg.value>=origPrice[tokenId]);
+     purchased[tokenId]=msg.sender;
+     _boughtTokens[msg.sender].add(tokenId);
    }
-
-   function purchase(uint256 tokenId) public tokenExists(tokenId) payable {
-     require(forSale[tokenId]==true && msg.value>=origPrice[tokenId],'error: not for sale or not enough eth');
-     //purchased[tokenId]=msg.sender;
-     _transfer(owner(), msg.sender, tokenId);
-     forSale[tokenId]=false;
-     //_boughtTokens[msg.sender].add(tokenId);
-   }
-
 
 
 /**
-TO DO:
-create a new contract with the buy sell functions
-add logs
-edit to match style good practices
+function keyReg() public view returns (address) {
+  return encKeyReg;
+}
 
 
+function BatchApply(uint256 batchId, string memory _text, bytes4 methodId) internal  {
+ uint256 size = batchSize(batchId);
+ uint256 _i = 0;
+ while (_i<size) {
+   address(this).call(abi.encode(methodId, _text,_batchSet[batchId].at(_i)));
+   _i++;
+ }
+}
 
 function setBatchHash(uint256 batchId, string memory _hashed) public onlyOwner {
   BatchApply(batchId,  _hashed, bytes4(keccak256("setSecretHash(uint256,string)")));
 }
+
+
+   function readEncKey(address _var) public returns (bytes memory) {
+     bool status;
+     bytes memory result;
+     address _reg = keyReg();
+     (status,result) = _reg.delegatecall(abi.encodePacked(bytes4(keccak256("getKey(address)")), _var));
+     emit LogEncKeyRead( _var ,  result, status);
+     return result;
+
+    }
 
 **/
 
