f2a881d04cba488bda3e9ecf5a281dbeb57a87ca
martin112112 <20109376+martin112112@users.noreply.github.com>
2020-12-05 10:37:17 +0100

    backup before adding one more contract layer

--- contracts-truffle/contracts/NFTcrypt.sol
+++ contracts-truffle/contracts/NFTcrypt.sol
@@ -3,10 +3,16 @@ pragma solidity  >=  0.4.25;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "NFTcryptTools.sol";
 
 
 contract NFTcrypt is Ownable {
   using EnumerableSet for EnumerableSet.AddressSet;
+  using SafeMath for uint256;
+  using Address for address;
+  using EnumerableSet for EnumerableSet.UintSet;
+  using EnumerableMap for EnumerableMap.UintToAddressMap;
+  using Strings for uint256;
 
   address public registry;
   function setRegistry(address reg) public onlyOwner {
@@ -15,8 +21,6 @@ contract NFTcrypt is Ownable {
 
     event LogCreatedChild(address sender, string arg, string ticker, address created, address newOwner);
     mapping (address => EnumerableSet.AddressSet) private _deployedSet;
-
-
 
     function createChild(string memory name, string memory abb) public payable {
         address newOwner = msg.sender;
@@ -29,10 +33,15 @@ contract NFTcrypt is Ownable {
     }
 }
 
-contract Child is ERC721,Ownable {
+contract Child is ERC721,Ownable,NFTcryptTools {
+  using SafeMath for uint256;
+  using Address for address;
+  using EnumerableSet for EnumerableSet.UintSet;
+  using EnumerableMap for EnumerableMap.UintToAddressMap;
+  using Strings for uint256;
 
    //event LogCreatedBy(address creator, string name, string ticker);
-   // address public encKeyReg;
+
    //string public ownerName;
 
    constructor (string memory name, string memory abb, address newOwner) ERC721(name, abb) public payable {
@@ -42,24 +51,15 @@ contract Child is ERC721,Ownable {
         //emit LogCreatedBy(msg.sender, name, abb);
    }
    //string ownerName;
-   uint256 maxIndex = 0;
-   uint256 lastBatch = 0;
+   uint256 public maxIndex = 0;
+   uint256 public lastBatch = 0;
 
    mapping (uint256 => uint256) public batchReg;
    mapping (uint256 => EnumerableSet.UintSet) _batchSet;
    mapping (uint256 => uint256) public batchMax;
-   //mapping (uint256 => string) private _encrSecret;
-   mapping (uint256 => string) public _secretHash;
-  // mapping (uint256 => bool) private _batchIdentical;
-   //mapping (uint256 => address) private purchased;
-   //mapping (address => EnumerableSet.UintSet) private _boughtTokens;
-   mapping (uint256 => uint256) public origPrice;
-   mapping (uint256 => bool) public forSale;
 
-   modifier tokenExists(uint256 tokenId) {
-     require(_exists(tokenId), "ERC721:  nonexistent token");
-     _;
-   }
+
+
 /**
    function getOwnerName() public view returns (string memory) {
      return ownerName;
@@ -77,31 +77,22 @@ contract Child is ERC721,Ownable {
         i++;
      }
    }
-   function createNewBatch(uint256 numberOfNft) onlyOwner public  {
-     uint256 batchId = lastBatch+1;
-     _issueNFT(numberOfNft,batchId);
-     lastBatch = batchId;
-     batchMax[batchId] = numberOfNft;
+   function addOrNewBatch(uint256 numberOfNft, uint256 batchId) onlyOwner public  {
+     if (lastBatch>=batchId){
+       _issueNFT(numberOfNft,batchId);
+       batchMax[batchId] = batchMax[batchId] + numberOfNft;
+     } else {
+       _issueNFT(numberOfNft,lastBatch+1);
+       lastBatch = lastBatch +1;
+       batchMax[lastBatch] = numberOfNft;
+     }
+
    }
    /**
-   function batchExists(uint256 batchId) public view returns (bool) {
-     if (lastBatch>=batchId) {
-       return true;
-     } else {
-       return false;
-     }
-   } */
-
-   function addToBatch(uint256 numberOfNft, uint256 batchId) onlyOwner public  {
-     require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
-     _issueNFT(numberOfNft,batchId);
-     batchMax[batchId] = batchMax[batchId] + numberOfNft;
-   }
-
    function batchSize(uint256 batchId) public view returns (uint256)  {
      require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
      return batchMax[batchId];
-   }
+   }**/
 
    function setBatchTokenURI(uint256 batchId, string memory _tokenURI) onlyOwner public  {
     require(lastBatch>=batchId, "ERC721: operator query for nonexistent batch");
@@ -113,56 +104,31 @@ contract Child is ERC721,Ownable {
    }
 
    function newBatchWithURI(uint256 numberOfNft,string memory _tokenURI) onlyOwner public {
-     createNewBatch(numberOfNft);
+     addOrNewBatch(numberOfNft,lastBatch+1);
      setBatchTokenURI(lastBatch,_tokenURI);
    }
 
    function setTokenURI(uint256 tokenId, string memory _tokenURI) public tokenExists(tokenId) onlyOwner {
      _setTokenURI(tokenId,_tokenURI);
    }
-/**
-   function getBatch(uint256 tokenId) public view returns (uint256) {
-     return batchReg[tokenId];
-
-   } **/
-
-
-
-   /** function setEncSecret(uint256 tokenId, string memory secret) public tokenExists(tokenId) onlyOwner {
-     _encrSecret[tokenId] = secret;
-   }**/
-    function setSecretHash(uint256 tokenId, string memory hashed) public onlyOwner {
-     _secretHash[tokenId] = hashed;
-   }
-/**
-   function viewEncSecret(uint256 tokenId) public view tokenExists(tokenId) returns (string memory) {
-     return _encrSecret[tokenId];
-   }**/
    /**
-   function viewSecretHash(uint256 tokenId) public view returns (string memory) {
-     return _secretHash[tokenId];
+   function getBatch(uint256 tokenId) public view tokenExists(tokenId) returns (uint256) {
+     return batchReg[tokenId];
+   }
+
+
+
+    function setEncSecret(uint256 tokenId, string memory secret) public tokenExists(tokenId) onlyOwner {
+     _setEncSecret(tokenId,secret);
    } **/
-/**
-    function buyer(uint256 tokenId) public view returns(address) {
-     return purchased[tokenId];
+    function setSecretHash(uint256 tokenId, string memory hashed) public tokenExists(tokenId) onlyOwner {
+     _setSecretHash(tokenId,hashed);
    }
-   **/
 
 
-   function getPrice(uint256 tokenId) public view tokenExists(tokenId) returns(uint256) {
-     require(forSale[tokenId]==true);
-     return  origPrice[tokenId];
-   }
-   function checkForSale(uint256 tokenId) public view tokenExists(tokenId) returns (bool) {
-     if (forSale[tokenId]==true) {
-       return true;
-     } else {
-       return false;}
-   }
-   function setPrice(uint256 tokenId, uint256 price) public tokenExists(tokenId) onlyOwner {
+   function setPrice(uint256 tokenId, uint256 price) public tokenExists(tokenId) {
      require(ownerOf(tokenId)==msg.sender);
-     origPrice[tokenId]=price;
-     forSale[tokenId]=true;
+     _setPrice(tokenId, price);
    }
 
    function setBatchPrice(uint256 batchId, uint256 price) onlyOwner public  {
@@ -176,10 +142,8 @@ contract Child is ERC721,Ownable {
 
    function purchase(uint256 tokenId) public tokenExists(tokenId) payable {
      require(forSale[tokenId]==true && msg.value>=origPrice[tokenId],'error: not for sale or not enough eth');
-     //purchased[tokenId]=msg.sender;
      _transfer(owner(), msg.sender, tokenId);
      forSale[tokenId]=false;
-     //_boughtTokens[msg.sender].add(tokenId);
    }
 
 
@@ -190,11 +154,6 @@ create a new contract with the buy sell functions
 add logs
 edit to match style good practices
 
-
-
-function setBatchHash(uint256 batchId, string memory _hashed) public onlyOwner {
-  BatchApply(batchId,  _hashed, bytes4(keccak256("setSecretHash(uint256,string)")));
-}
 
 **/
 
